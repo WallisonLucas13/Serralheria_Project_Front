86af25f65265ad0efb89b4f8637be498035b997b
WallisonLucas13 <lucaswalison136@gmail.com>
2023-04-13 20:20:31 -0300

    release

--- src/app/Auth/login/login.service.ts
+++ src/app/Auth/login/login.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { AuthenticationResponse } from 'src/app/models/AuthenticationResponse';
@@ -8,11 +8,14 @@ import { AuthenticationResponse } from 'src/app/models/AuthenticationResponse';
 })
 export class LoginService {
 
-  private API_POST: string  = "https://serralheriaproject-production.up.railway.app/api/login"
+  private API_POST: string  = "https://serralheriaproject-production.up.railway.app/api/login";
+  private HEADER_FULL: HttpHeaders = new HttpHeaders();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.HEADER_FULL = this.HEADER_FULL.set("Content-Type", "application/json");
+   }
 
   sendLogin(form: FormGroup){
-    return this.http.post<AuthenticationResponse>(this.API_POST, form.value, {observe: 'body'});
+    return this.http.post<AuthenticationResponse>(this.API_POST, form.value, {observe: 'body', headers: this.HEADER_FULL});
   }
 }
