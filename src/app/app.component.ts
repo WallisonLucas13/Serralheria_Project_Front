8cc6fe72616612e8951381633ef1f6db0c71ef77
WallisonLucas13 <lucaswalison136@gmail.com>
2023-04-14 10:49:03 -0300

    release

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoginService } from './Auth/login/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +11,26 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'clientes-crud';
+  formVerifyLogged: FormGroup | undefined;
 
-  constructor(private route: Router){
+  constructor(private route: Router, private loginService: LoginService, private toast: ToastrService){
 
     const logged = localStorage.getItem("token");
     if(!logged){
       this.route.navigateByUrl("user/login");
+    }
+    else{
+      this.formVerifyLogged = new FormGroup(
+        {token: new FormControl(logged)}
+      );
+      this.loginService.sendLoginWithToken(this.formVerifyLogged)
+        .catch(() => {
+          this.toast.warning("Acesso Expirado, Entre Novamente!", "",{
+            timeOut: 2000,
+            positionClass: "toast-bottom-center"
+          });
+          setTimeout(() => this.route.navigateByUrl("user/login"),2000);
+        })
     }
   }
 
