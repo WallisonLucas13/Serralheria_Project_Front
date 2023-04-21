ca4dbe78cb61940017a76c9eb391be523100f574
WallisonLucas13 <lucaswalison136@gmail.com>
2023-04-21 11:56:24 -0300

    release

--- src/app/Auth/login/login.service.ts
+++ src/app/Auth/login/login.service.ts
@@ -11,8 +11,8 @@ import { AuthenticationResponse } from 'src/app/models/AuthenticationResponse';
 })
 export class LoginService {
 
-  private API_POST: string  = "https://serralheriaproject-production.up.railway.app/api/login";
-  private API_POST_ACCESS: string  = "https://serralheriaproject-production.up.railway.app/api/login/access";
+  private API_POST: string = "https://serralheriaproject-production.up.railway.app/api/login";
+  private API_POST_ACCESS: string = "https://serralheriaproject-production.up.railway.app/api/login/access";
   public token: string = "";
   private tokenForm: FormGroup;
 
@@ -22,34 +22,36 @@ export class LoginService {
     this.tokenForm = new FormGroup({
       token: new FormControl(this.token)
     });
-   }
+  }
 
   //Realiza Login com as Credenciais Básicas
-  public sendLogin(form: FormGroup){
+  public sendLogin(form: FormGroup) {
     return axios.post<AuthenticationResponse>(this.API_POST, form.value);
   }
 
   //Verifica as permissões de Usuário *Método Utilizado em todas as Rotas
-  public sendLoginWithToken(){
-    this.tokenForm = new FormGroup({token: new FormControl(this.token)});
+  public sendLoginWithToken() {
+    this.tokenForm = new FormGroup({ token: new FormControl(this.token) });
 
     axios.post(this.API_POST_ACCESS, this.tokenForm.value)
-    .then(() => {
-      if(location.pathname == "/user/login"){
-        this.toast.success("Bem vindo de volta!", "",{
-          positionClass: "toast-bottom-center",
-          timeOut: 2000
-        })
-        setTimeout(() => this.router.navigateByUrl("clientes"), 2000);
-      }
-    })
-    .catch(() => {
-      this.toast.warning("Acesso Expirado, Entre Novamente!", "",{
-        timeOut: 2000,
-        positionClass: "toast-bottom-center"
-      });
-      localStorage.clear();
-      setTimeout(() => this.router.navigateByUrl("user/login"),2000);
-    })
+      .then(() => {
+        if (location.pathname == "/user/login") {
+          this.toast.success("Bem vindo de volta!", "", {
+            positionClass: "toast-bottom-center",
+            timeOut: 2000
+          })
+          setTimeout(() => this.router.navigateByUrl("clientes"), 2000);
+        }
+      })
+      .catch(() => {
+        if (location.pathname != "/user/login") {
+          this.toast.warning("Acesso Expirado, Entre Novamente!", "", {
+            timeOut: 2000,
+            positionClass: "toast-bottom-center"
+          });
+          setTimeout(() => this.router.navigateByUrl("user/login"), 2000);
+        }
+        localStorage.clear();
+      })
   }
 }
